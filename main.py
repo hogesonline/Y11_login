a1a7af7dddd9c01de9726b4484474ad75ce61151
Amanda Hogan <hogesonline@gmail.com>
2022-09-06 12:07:18 +0000

    Reset to pre-solution

--- main.py
+++ main.py
@@ -1,47 +1,50 @@
-'''
-Step 1
+
+#Step 1
 username = "username"
 password = "password"
-entered_uname = input("What is your username? ")
-entered_pwd = input("What is your password? ")
-if username == entered_uname and password == entered_pwd:
-  print("You are verified!")
+entered_uname = #get input
+entered_pwd = #get input
+if <condition to check username> and <condition to check password>:
+  #display appropriate message
 else:
-  print("Try again")
-'''
+  #display appropriate message
+
 #Step 2 - do the same thing with a function
 '''
 username = "username"
 password = "password"
 
-def login():
+def <function name>():
   #username and password defined in main program
   global username, password
-  entered_uname = input("What is your username? ")
-  entered_pwd = input("What is your password? ")
-  if username == entered_uname and password == entered_pwd:
-    print("You are verified!")
+  entered_uname = #get input
+  entered_pwd = #get input
+  if <condition to check username> and <condition to check password>:
+    #display appropriate message
   else:
-    print("Try again")
+    #display appropriate message
 
+#call the function
 login()
 '''
 
 #Step 2A
-
+'''
 username = "username"
 password = "password"
 
 
-def login(entered_uname, entered_pwd):
-    #username and password defined in main program
-    global username, password
-    if username == entered_uname and password == entered_pwd:
-        return True
-    else:
-        return False
+def <function name>(<name of first variable>, <name of second variable>):
+  #username and password defined in main program
+  global username, password
+  if <condition to check username> and <condition to check password>:
+    #return appropriate message
+  else:
+    #return appropriate message
 
 
 #a = input("What is your username? ")
 #b = input("What is your password? ")
+#get input and call the function
 print(login("password", "username"))
+'''
